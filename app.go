2fa0ad1c4ace91544d5b85c41fd0e7ad77743aaa
shaninalex <shanin.x@gmail.com>
2023-04-15 09:11:35 +0300

    base app structures

--- app.go
+++ app.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"log"
 )
 
 type App struct {
 	DB *sql.DB
+	ui *Ui
 }
 
 func (app *App) Init(dbpath string) {
@@ -14,4 +16,25 @@ func (app *App) Init(dbpath string) {
 		panic(err)
 	}
 	app.DB = db
+
+	app.ui = &Ui{}
+
+	app.ui.UIInit(500, 300)
+
+}
+
+func (app *App) Run() {
+
+	// check amount of projects.
+	projects, err := GetAllProjects(app.DB)
+	if err != nil {
+		log.Fatal(nil)
+	}
+	if len(projects) == 0 {
+		// show timer
+		app.ui.UICreateProjectWindow()
+	} else {
+		// show create project window
+		app.ui.UICreateTimerWindow()
+	}
 }
--- ui.go
+++ ui.go
@@ -1,17 +1,28 @@
 package main
 
-import "fyne.io/fyne"
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+)
 
 type Ui struct {
-	size fyne.Size
-
-	window fyne.Window
+	Size        fyne.Size
+	Application fyne.App
+	Window      fyne.Window
 }
 
-func (ui *Ui) init(width, height int) {
-	ui.size = fyne.NewSize(width, height)
+func (ui *Ui) UIInit(width, height int) {
+	ui.Size = fyne.NewSize(width, height)
+
+	ui.Application = app.New()
+	ui.Window = ui.Application.NewWindow("hello")
+	ui.Window.Resize(ui.Size)
 }
 
-func (ui *Ui) CreateProjectWindow() {
+func (ui *Ui) UICreateProjectWindow() {
+
+}
+
+func (ui *Ui) UICreateTimerWindow() {
 
 }
